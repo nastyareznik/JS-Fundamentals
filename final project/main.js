63f8d1a83f362e7d424eadcc88465c60078c65e8
nastyareznik <rezansty@gmail.com>
2023-06-07 09:13:42 +0300

    add class Game to main.js

--- final project/main.js
+++ final project/main.js
@@ -1,44 +1,61 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var gameContainer = document.getElementById("game-container");
-  var containerWidth = gameContainer.clientWidth;
-  var containerHeight = gameContainer.clientHeight;
-  var score = 0;
-  var lives = 3;
-  var speed = 2000;
-  var level = "Easy";
+  class Game {
+    constructor() {
+      this.gameContainer = document.getElementById("game-container");
+      this.containerWidth = this.gameContainer.clientWidth;
+      this.containerHeight = this.gameContainer.clientHeight;
+      this.score = 0;
+      this.lives = 3;
+      this.speed = 2000;
+      this.level = "Easy";
+      this.intervalId = null;
+      this.gameOver = false;
+    }
 
-  function startGame() {
-    score = 0;
-    gameContainer.innerHTML = "";
-    updateStats();
+    startGame() {
+      this.score = 0;
+      this.gameContainer.innerHTML = "";
+      this.updateStats();
 
-    var intervalId;
-    var gameOver = false;
+      this.intervalId = setInterval(() => {
+        this.createTarget();
+      }, this.speed);
 
-    function createTarget() {
-      if (!gameOver) {
-        var target = document.createElement("div");
+      setTimeout(() => {
+        clearInterval(this.intervalId);
+        this.gameOver = true;
+        this.showGameOverMessage();
+      }, 30000);
+    }
+
+    createTarget() {
+      if (!this.gameOver) {
+        const target = document.createElement("div");
         target.classList.add("target");
         target.style.backgroundImage = "url('../img/cat-face.png')";
         target.style.top =
-          Math.floor(Math.random() * (containerHeight - 50)) + "px";
+          Math.floor(Math.random() * (this.containerHeight - 50)) + "px";
         target.style.left =
-          Math.floor(Math.random() * (containerWidth - 50)) + "px";
-        gameContainer.appendChild(target);
+          Math.floor(Math.random() * (this.containerWidth - 50)) + "px";
+        this.gameContainer.appendChild(target);
 
-        target.addEventListener("click", function () {
-          score++;
-          gameContainer.removeChild(target);
+        target.addEventListener("click", () => {
+          this.score++;
+          this.gameContainer.removeChild(target);
         });
 
-        var animationId = setInterval(function () {
-          var targetTop = parseInt(target.style.top);
-          var targetLeft = parseInt(target.style.left);
-          var randomTop = Math.floor(Math.random() * (containerHeight - 50));
-          var randomLeft = Math.floor(Math.random() * (containerWidth - 50));
-          var distanceX = Math.abs(targetLeft - randomLeft);
-          var distanceY = Math.abs(targetTop - randomTop);
-          var moveSpeed = getMoveSpeed();
+        const animationId = setInterval(() => {
+          const targetTop = parseInt(target.style.top);
+          const targetLeft = parseInt(target.style.left);
+          const randomTop = Math.floor(
+            Math.random() * (this.containerHeight - 50)
+          );
+          const randomLeft = Math.floor(
+            Math.random() * (this.containerWidth - 50)
+          );
+          const distanceX = Math.abs(targetLeft - randomLeft);
+          const distanceY = Math.abs(targetTop - randomTop);
+          const moveSpeed = this.getMoveSpeed();
 
           if (targetTop < randomTop) {
             target.style.top = targetTop + moveSpeed + "px";
@@ -54,40 +71,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
           if (distanceX < moveSpeed && distanceY < moveSpeed) {
             clearInterval(animationId);
-            gameContainer.removeChild(target);
+            this.gameContainer.removeChild(target);
           }
         }, 10);
       }
     }
 
-    function checkCollision(bullet) {
-      var targets = document.getElementsByClassName("target");
-      var bulletRect = bullet.getBoundingClientRect();
-
-      for (var i = 0; i < targets.length; i++) {
-        var target = targets[i];
-        var targetRect = target.getBoundingClientRect();
-
-        if (
-          bulletRect.top <= targetRect.bottom &&
-          bulletRect.right >= targetRect.left &&
-          bulletRect.left <= targetRect.right &&
-          bulletRect.bottom >= targetRect.top
-        ) {
-          score++;
-          gameContainer.removeChild(target);
-          gameContainer.removeChild(bullet);
-          break;
-        }
-      }
-    }
-
-    gameContainer.addEventListener("click", function (event) {
-      handleShot(event);
-    });
-
-    function getMoveSpeed() {
-      switch (level) {
+    getMoveSpeed() {
+      switch (this.level) {
         case "Easy":
           return 1;
         case "Medium":
@@ -99,65 +90,46 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
 
-    function updateStats() {
-      document.getElementById("lives").innerText = "Lives: " + lives;
+    updateStats() {
+      document.getElementById("lives").innerText = "Lives: " + this.lives;
     }
 
-    document.addEventListener("keydown", function (event) {
-      if (event.key === " ") {
-        handleShot({ clientX: containerWidth / 2, clientY: containerHeight });
-      }
-    });
-
-    document.getElementById("easy-btn").addEventListener("click", function () {
-      level = "Easy";
-      speed = 2000;
-      lives = 3;
-      updateStats();
-      startGame();
-    });
-
-    document
-      .getElementById("medium-btn")
-      .addEventListener("click", function () {
-        level = "Medium";
-        speed = 1500;
-        lives = 2;
-        updateStats();
-        startGame();
-      });
-
-    document.getElementById("hard-btn").addEventListener("click", function () {
-      level = "Hard";
-      speed = 1000;
-      lives = 1;
-      updateStats();
-      startGame();
-    });
-
-    document.getElementById("play-btn").addEventListener("click", function () {
-      intervalId = setInterval(function () {
-        createTarget();
-      }, speed);
-    });
-
-    setTimeout(function () {
-      clearInterval(intervalId);
-      gameOver = true; // гру завершено
-      showGameOverMessage();
-    }, 30000);
-
-    // повідомлення про закінчення гри
-    function showGameOverMessage() {
-      if (gameOver) {
-        var result = document.createElement("div");
-        result.innerText = "Game Over! You caught " + score + " cats";
+    showGameOverMessage() {
+      if (this.gameOver) {
+        const result = document.createElement("div");
+        result.innerText = "Game Over! You caught " + this.score + " cats";
         result.classList.add("result");
         result.style.display = "block";
-        gameContainer.appendChild(result);
+        this.gameContainer.appendChild(result);
       }
     }
   }
 
-  startGame();
+  const game = new Game();
+  game.startGame();
+
+  document.getElementById("easy-btn").addEventListener("click", function () {
+    game.level = "Easy";
+    game.speed = 2000;
+    game.lives = 3;
+    game.updateStats();
+    game.startGame();
+  });
+
+  document.getElementById("medium-btn").addEventListener("click", function () {
+    game.level = "Medium";
+    game.speed = 1500;
+    game.lives = 2;
+    game.updateStats();
+    game.startGame();
+  });
+
+  document.getElementById("hard-btn").addEventListener("click", function () {
+    game.level = "Hard";
+    game.speed = 1000;
+    game.lives = 1;
+    game.updateStats();
+    game.startGame();
+  });
 });
+
